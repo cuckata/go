125e183dcc2c77b14b482271ac1fc132cbae69a8
cuckata <vcvetomir@yahoo.com>
2025-02-24 21:02:01 +0200

    interfaces, http and practice

---
+++ interfaces/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (eb englishBot) getGreeting() string {
+	//Very custom logic for english bot
+	return "Hi there!"
+}
+
+func (sb spanishBot) getGreeting() string {
+	//Very custom logic for spanish bot
+	return "Hola!"
+}
